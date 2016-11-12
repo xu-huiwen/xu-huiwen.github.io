37b52c22490f6a8c621a57791ea7c360747a13b4
xu-huiwen <1710513618@qq.com>
2016-11-12 08:33:11 +0800

    个人主页

--- js/me.js
+++ js/me.js
@@ -1,31 +1,3 @@
 $(document).ready(function(){
-	// 楼层跳转
-
-	var cheight=document.documentElement.clientHeight;
-	var nheight;
-	var now;
-	for (var i = 0; i < $(".floor").length; i++) {
-		var juli=$(".floor").eq(i).position();
-		$(".floor").eq(i).prop("h",juli.top);
-	}
-	$(window).on("scroll",function(){
-		var top=$(window).scrollTop()
-		if(top>=$(".floor").first().prop("h")){
-		}
-})
-
-$(".lo").on("click",function(){
-			var index=$(this).index();
-			var hh=$(".floor").eq(index).prop("h")
-			var top=$(window).scrollTop()
-			var src={aa:top}
-			$(src).animate({aa:hh},{
-				duration:300,
-				step:function(){
-					$(window).scrollTop(src.aa)
-				}
-			})
-			now=index;
-		})
-
+	
 })
